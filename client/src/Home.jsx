5b28a563e8e681d716bb2b572159d27bcce30f47
TuhinPramanik4 <tuhinp475@gmail.com>
2025-05-25 10:52:17 +0530

    Create Navbar

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Navbar from './Home_Components/Navbar'
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-gray-400'>Home</div>
+    <div className='w-full h-screen bg-green-400'>
+      <Navbar/>  
+    </div>
   )
 }
---
+++ client/src/Home_Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="w-full h-[12vh] bg-white shadow flex items-center">
+  {/* Left: Logo + Brand */}
+  <div className='w-1/4 h-full  flex  justify-center items-center'>
+                <div className='w-1/3 h-full flex justify-center items-center '>
+                <img src="./assets/logo" alt="logo" />
+                </div>
+                <div className='w-2/3 text-2xl h-full flex justify-center items-center'>
+                <h2>Star Properties</h2>
+                </div>
+        </div>
+
+  {/* Right: Navigation */}
+  <div className="w-2/4 h-full flex items-center justify-center   bg-white">
+    <ul className="flex  text-lg font-medium gap-30 text-gray-700">
+      <li className="hover:text-blue-600 transition-colors cursor-pointer">Buy</li>
+      <li className="hover:text-blue-600 transition-colors cursor-pointer">Rent</li>
+      <li className="hover:text-blue-600 transition-colors cursor-pointer">PG</li>
+      <li className="hover:text-blue-600 transition-colors cursor-pointer">Plots</li>
+    </ul>
+  </div>
+  <div className='w-1/4 h-full  flex items-center justify-center   bg-white'>
+   <button className="ml-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+          Sign In
+        </button></div>
+</div>
+
+
+  )
+}
+
+export default Navbar
