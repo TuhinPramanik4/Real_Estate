f44cd773c3fb0b4cc58128ff1fa45b9dd6d57e87
TuhinPramanik4 <tuhinp475@gmail.com>
2025-05-25 13:02:09 +0530

    Modify search component

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import Navbar from './Home_Components/Navbar'
+import Navbar from './Home_Components/Navbar';
+import Search from './Home_Components/Search';
+
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-green-400'>
-      <Navbar/>  
+    <div className="w-full h-screen bg-green-500">
+      <Navbar />
+      
+      {/* Center the Search component */}
+      <div className="w-full h-[calc(100vh-12vh)] flex justify-center items-top mt-15">
+        <Search />
+      </div>
     </div>
-  )
+  );
 }
