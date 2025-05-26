da8c4658ec53da8c1cbe7668481d89c48719a56a
TuhinPramanik4 <tuhinp475@gmail.com>
2025-05-26 21:41:36 +0530

    Modify Home.jsx

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,14 +1,20 @@
 import Navbar from './Home_Components/Navbar';
 import Search from './Home_Components/Search';
-
+import Lists from './Home_Components/Lists';
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-green-500">
+    <div className="w-full h-screen bg-white">
       <Navbar />
       
       {/* Center the Search component */}
-      <div className="w-full h-[calc(100vh-12vh)] flex justify-center items-top mt-15">
-        <Search />
+      <div className="w-full h-[calc(100vh-12vh)] flex flex-col justify-center items-top">
+          <div className='w-full h-1/2 flex justify-center items-top mt-15 bg-red-400'>
+             <Search />
+          </div>
+          <div className='w-full h-1/2 flex justify-center items-top'>
+             <Lists/>
+          </div>
+
       </div>
     </div>
   );
--- client/src/Home_Components/Navbar.jsx
+++ client/src/Home_Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="w-full h-[12vh] bg-white shadow flex items-center">
+    <div className="w-full h-[12vh] text-white bg-black shadow flex items-center">
   {/* Left: Logo + Brand */}
   <div className='w-1/4 h-full  flex  justify-center items-center'>
                 <div className='w-1/3 h-full flex justify-center items-center '>
@@ -14,15 +14,15 @@ function Navbar() {
         </div>
 
   {/* Right: Navigation */}
-  <div className="w-2/4 h-full flex items-center justify-center   bg-white">
-    <ul className="flex  text-lg font-medium gap-30 text-gray-700">
+  <div className="w-2/4 h-full flex items-center justify-center   bg-black">
+    <ul className="flex  text-lg font-medium gap-30 text-white">
       <li className="hover:text-blue-600 transition-colors cursor-pointer">Buy</li>
       <li className="hover:text-blue-600 transition-colors cursor-pointer">Rent</li>
       <li className="hover:text-blue-600 transition-colors cursor-pointer">PG</li>
       <li className="hover:text-blue-600 transition-colors cursor-pointer">Plots</li>
     </ul>
   </div>
-  <div className='w-1/4 h-full  flex items-center justify-center   bg-white'>
+  <div className='w-1/4 h-full  flex items-center justify-center   bg-blacl'>
    <button className="ml-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
           Sign In
         </button></div>
